9773a807ed777822edce846f959de2f841e27e0f
Edward Muturi <edward@Edwards-MacBook-Pro.local>
2019-07-14 19:30:47 +0300

    basic UI done

---
+++ app/src/main/java/com/edward/alcabout/AboutAlcActivity.java
@@ -0,0 +1,14 @@
+package com.edward.alcabout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutAlcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+    }
+}
