686c7562b21cf135e2ca8d52311a9469fb49507c
Edward Muturi <edward@Edwards-MacBook-Pro.local>
2019-07-14 20:20:05 +0300

    - Implement WebView - Change colors to match andela theme - Override onReceivedSslError to fix WebView not displaying error - Add Internet permission

--- app/src/main/java/com/edward/alcabout/AboutAlcActivity.java
+++ app/src/main/java/com/edward/alcabout/AboutAlcActivity.java
@@ -2,16 +2,62 @@ package com.edward.alcabout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutAlcActivity extends AppCompatActivity {
+
+    private WebView alcWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
+
+        alcWebView = findViewById(R.id.webview_about_alc);
+        alcWebView.getSettings().setLoadsImagesAutomatically(true);
+        alcWebView.getSettings().setJavaScriptEnabled(true);
+        alcWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+//        alcWebView.setWebViewClient(new WebViewClient());
+        alcWebView.loadUrl("https://andela.com/alc/");
+
+        alcWebView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog = new ProgressDialog(AboutAlcActivity.this);
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressDialog.setTitle("Loading...");
+                progressDialog.setMessage("Please wait...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+            }
+
+            @Override
+            public void onPageCommitVisible(WebView view, String url) {
+                super.onPageCommitVisible(view, url);
+                if (progressDialog != null) {
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed(); // Ignore SSL certificate errors
+            }
+        });
+        alcWebView.getSettings().setLoadWithOverviewMode(true);
+//        Enable responsive layout
+        alcWebView.getSettings().setUseWideViewPort(true);
+//        Zoom out if content width is greater
     }
 
 }
